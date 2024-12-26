8eabe96a04ba8c4b5d20bdc7a8482100260918a6
Manikesh <manikesh19dec@gmail.com>
2024-12-26 21:49:25 +0530

    Added get user by id in repo

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const prepareAndStartServer= () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api',apiRoutes);
-    app.listen(PORT, ()=> {
-        console.log(`Server started on Port: ${PORT}`)
+    app.listen(PORT, async()=> {
+        console.log(`Server started on Port: ${PORT}`);
     })
 }
 
