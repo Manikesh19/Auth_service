ba88e6ede264675cada3bfd6c2c9eb512db9e2ac
Manikesh <manikesh19dec@gmail.com>
2025-01-09 22:19:33 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const bodyParser= require('body-parser');
 const { PORT } = require('./config/serverConfig');
 const apiRoutes= require('./routes/index');
 
+const UserService = require('./services/user-services');
+
 const prepareAndStartServer= () => {
 
     
@@ -13,6 +15,13 @@ const prepareAndStartServer= () => {
     app.use('/api',apiRoutes);
     app.listen(PORT, async()=> {
         console.log(`Server started on Port: ${PORT}`);
+
+        const service= new UserService();
+        // const newToken= service.createToken({email:'manikesh123@gmail.com', id:1});
+        // console.log("New token is",newToken);
+        const token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1hbmlrZXNoMTIzQGdtYWlsLmNvbSIsImlkIjoxLCJpYXQiOjE3MzY0NDEwNjcsImV4cCI6MTczNjQ0MTA5N30.JbKs5NDKS8rR1UzcuU-6gyHpGj2-qP10prYOU3LuWDE';
+        const response= service.verifyToken(token);
+        console.log(response);
     })
 }
 
--- src/services/user-services.js
+++ src/services/user-services.js
@@ -1,3 +1,5 @@
+const jwt =require('jsonwebtoken');
+const {JWT_KEY}= require('../config/serverConfig');
 const UserRepository = require('../repository/user-repository');
 
 class UserServices {
@@ -22,6 +24,28 @@ class UserServices {
         } catch (error) {
             console.log("Something went wrong in services layer");
             throw{error}
+        }
+    }
+
+    createToken(user) {
+        try {
+            const result= jwt.sign(user,JWT_KEY,{expiresIn:'1d'});
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw {error}
+        }
+    }
+
+
+    verifyToken(token) {
+        try {
+            const response= jwt.verify(token,JWT_KEY);
+            return response;
+
+        } catch (error) {
+            console.log("Something went wrong in token validation",error);
+            throw {error}
         }
     }
 }
