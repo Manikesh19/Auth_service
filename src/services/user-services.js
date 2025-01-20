3502a51d22cc26e3872518ab41a17c7fee257e2f
Manikesh <manikesh19dec@gmail.com>
2025-01-20 19:57:06 +0530

    Added user isAuthenticated function

--- src/services/user-services.js
+++ src/services/user-services.js
@@ -44,6 +44,22 @@ class UserServices {
         }
     }
 
+    async isAuthenticated(token) {
+        try {
+            const response= this.verifyToken(token);
+            if(!response) {
+                throw {error: 'Invalid token'}
+            }
+            const user= this.userRepository.getById(response.id);
+            if(!user) {
+                throw {error: 'No user corresponding to the token exists'}
+            }
+            return user.id;
+        } catch (error) {
+            console.log("Something went wrong in sign in process");
+            throw error;
+        }
+    }
     createToken(user) {
         try {
             const result= jwt.sign(user,JWT_KEY,{expiresIn:'1d'});
